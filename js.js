6c4143cd2a552b8e4c467dd2a26a5b7aa928a4ac
Didymus <didymus.benson@gmail.com>
2017-08-17 11:58:06 -0600

    changed directions to use an angular approach.

--- js.js
+++ js.js
@@ -1,11 +1,26 @@
 $(document).ready(function () {
-            $(".submenu").click(function () {
-                console.log("wtf?")
-                if ($(this).hasClass("largeWidth")) {
-                    $(".submenu").removeClass('largeWidth');
-                } else {
-                    $(".submenu").removeClass('largeWidth');
-                    $(this).toggleClass('largeWidth');
-                }
-            });
-        });
+    $(".submenu").click(function () {
+        console.log("wtf?")
+        if ($(this).hasClass("largeWidth")) {
+            $(".submenu").removeClass('largeWidth');
+        } else {
+            $(".submenu").removeClass('largeWidth');
+            $(this).toggleClass('largeWidth');
+        }
+    });
+});
+
+// APP
+
+
+
+var khApp = angular.module('khApp', []);
+khApp.controller('mainController', function ($scope) {
+
+    // create a message to display in our view
+    $scope.message = 'Testing 123.';
+
+    $scope.menus = ["KHFM","KH2FM","BBS"];
+
+
+});
