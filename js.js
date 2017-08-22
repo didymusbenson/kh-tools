06194ebe3de92c689483842a7db9b3dfbd8de8fd
Didymus <didymus.benson@gmail.com>
2017-08-22 09:57:28 -0600

    $#@% angular.

--- js.js
+++ js.js
@@ -1,23 +1,10 @@
-$(document).ready(function () {
-    $(".submenu").click(function () {
-        console.log("wtf?")
-        if ($(this).hasClass("largeWidth")) {
-            $(".submenu").removeClass('largeWidth');
-        } else {
-            $(".submenu").removeClass('largeWidth');
-            $(this).toggleClass('largeWidth');
-        }
-    });
-});
 
 
 var khApp = angular.module('khApp', []);
 khApp.controller('mainController', function ($scope) {
 
-    // create a message to display in our view
-    $scope.message = 'Testing 123.';
 
-    $scope.menus = [{"name": "KH:FM", "id":"hkfm"},{"name": "KH2:FM", "id":"hk2fm"},{"name": "Birth by Sleep", "id":"bbs"},{"name": "Dream Drop Distance", "id":"ddd"}];
+    $scope.menus = [{"name": "KH:FM", "id":"khfm"},{"name": "KH2:FM", "id":"kh2fm"},{"name": "Birth by Sleep", "id":"bbs"},{"name": "Dream Drop Distance", "id":"ddd"}];
 
 
 });
