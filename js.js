b4ae54b40529595c47d837a4531736bd698afe37
Didymus <didymus.benson@gmail.com>
2017-08-23 08:49:23 -0600

    Modals done!

--- js.js
+++ js.js
@@ -1,10 +1,1 @@
-
-
-var khApp = angular.module('khApp', []);
-khApp.controller('mainController', function ($scope) {
-
-
-    $scope.menus = [{"name": "KH:FM", "id":"khfm"},{"name": "KH2:FM", "id":"kh2fm"},{"name": "Birth by Sleep", "id":"bbs"},{"name": "Dream Drop Distance", "id":"ddd"}];
-
-
-});
+//window.location.replace("#modal-one")
